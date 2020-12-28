aab5f7a22a9ca4d0491be6050a1fcf41478a308a
TosinDev <aladetosinalade@gmail.com>
2020-12-28 16:45:54 +0100

    Adding Global styles

--- web/src/App.js
+++ web/src/App.js
@@ -6,13 +6,24 @@ import GlobalStyle from '/components/GlobalStyle';
 // import routes
 import Pages from '/pages';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// configure API URI & cache
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+//configure Apollo Client
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
+
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle />
            <Pages />
-        </div>
-    )
-}
+        </ApolloProvider>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
