4100f38e785d110ab8ef76532af2a6519453c34c
TosinDev <aladetosinalade@gmail.com>
2021-01-01 17:07:04 +0100

    Home page query

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,12 +1,51 @@
-import React from 'react';
+// import the required libraries
+import { useQuery, gql } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
 
-import Button from '../components/Button';
+// our GraphQL query, stored as a variable
+const GET_NOTES = gql`
+    query NoteFeed($cursor: String) {
+        noteFeed(cursor: $cursor) {
+            cursor
+            hasNextPage
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+    `;
 
 const Home = () => {
+    // query hook
+    const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+    // if the data is loading, display a loading message
+    if (loading) return <p>Loading...</p>;
+    // if there is an error fetching the data, display an error message
+    if (error) return <p>Error!</p>;
+
+    //if the data is successful, display data in UI
     return (
         <div>
-            <p>This is the home page</p>
-            <Button>Click me!</Button>
+            {data.noteFeed.notes.map(note => (
+                <article key={note.id}>
+                    <img
+                        src={note.author.avatar}
+                        alt={`${note.author.username} avatar`}
+                        height='50px'
+                    />{' '}
+                    {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+                    <ReactMarkdown source={note.content} />
+                </article>
+            ))}
         </div>
     );
 };
