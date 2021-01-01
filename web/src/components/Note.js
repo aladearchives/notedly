9888135c141dbe023c882061f14875da7cdf9261
TosinDev <aladetosinalade@gmail.com>
2021-01-01 18:17:58 +0100

    Dynamic Note queries

--- web/src/components/Note.js
+++ web/src/components/Note.js
@@ -1,56 +1,16 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-// import the format utility from `date-fns`
-import { format } from 'date-fns';
-import styled from 'styled-components';
-
-// Keep notes from extending wider than 800px
-const StyledNote = styled.article`
-    max-width: 800px;
-    margin: 0 auto;
-`;
-// Style the note metadata
-const MetaData = styled.div`
-    @media (min-width: 500px) {
-        display: flex;
-        align-items: top;
-    }
-`;
-
-// add some space between the avatar and meta info
-const MetaInfo = styled.div`
-    padding-right: 1em;
-`;
-
-// align 'UserActions' to the right on large screens
-const UserActions = styled.div`
-    margin-left: auto;
-`;
-
-// update the date markup to format it as Month, Day, and Year
-{ format(note.createdAt, 'MMM Do YYYY') } Favorites: { ' ' }
-
 const Note = ({ note }) => {
     return (
-        <StyledNote>
-            <MetaData>
-                <MetaInfo>
-                    <img
-                        src={note.author.avatar}
-                        alt={`${note.author.username} avatar`}
-                        height="50px"
-                    />
-                </MetaInfo>
-                <MetaInfo>
-                    <em>by</em> {note.author.username} <br />
-                    {format(note.createdAt, 'MMM Do YYYY')}
-                </MetaInfo>
-                <UserActions>
-                    <em>Favorites:</em> {note.favoriteCount}
-                </UserActions>
-            </MetaData>
+        <article>
+            <img
+                src={note.author.avatar}
+                alt="{note.author.username} avatar"
+                height="50px"
+            />{' '}
+            {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
             <ReactMarkdown source={note.content} />
-        </StyledNote>
+        </article>
     );
 };
 export default Note;
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -8,6 +8,7 @@ import Layout from '../components/Layout';
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
+import NotePage from './note';
 
 const Pages = () => {
     return (
@@ -16,6 +17,7 @@ const Pages = () => {
                 <Route exact path='/' component={Home} />
                 <Route path='/mynotes' component={MyNotes} />
                 <Route path='/favorites' component={Favorites} />
+                <Route path="/note/:id" component={NotePage} />
             </Layout>
         </Router>
     );
