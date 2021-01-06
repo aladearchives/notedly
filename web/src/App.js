a20e033739d3ef9b1a84d1b193a93dbceaf16ee3
TosinDev <aladetosinalade@gmail.com>
2021-01-06 05:36:52 +0100

    Include Token in Headers for requests

--- web/src/App.js
+++ web/src/App.js
@@ -6,14 +6,29 @@ import GlobalStyle from '/components/GlobalStyle';
 // import routes
 import Pages from '/pages';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
 // configure API URI & cache
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({uri});
 const cache = new InMemoryCache();
+
+// check for a token and return the headers to the context
+const authLink = setContext((_, {headers}) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    };
+});
+
 //configure Apollo Client
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {},
     connectToDevTools: true
 });
 
